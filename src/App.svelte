edc1c5b9532731fc8b5872e3a232acde27aaa402
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2023-04-13 17:35:27 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type {Question} from './types';
+  import Chooser from './Chooser.svelte';
+  import Line from './Line.svelte';
+
+  let questions: Question[] = [];
+</script>
+
+<main class="container">
+  <h1>Sorter</h1>
+  <Chooser bind:questions />
+  {#each questions as line}
+    <Line bind:line />
+  {/each}
+</main>
---
+++ src/Chooser.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+  import {read, utils} from 'xlsx';
+  import type {Question} from './types';
+
+  export let questions: Question[] = [];
+
+  onMount(async () => {
+    const input = document.getElementById('questioner');
+    input.onchange = e => {
+      const file = (e.target as HTMLInputElement).files[0];
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = readerEvent => {
+        const content = readerEvent.target.result;
+        const wb = read(content);
+        questions = utils.sheet_to_json<Question>(wb.Sheets[wb.SheetNames[0]]);
+      }
+    }
+  });
+</script>
+
+<input type="file" id="questioner" name="questions" accept=".xlsx">
